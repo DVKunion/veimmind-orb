7b114abdadf6b07298d599a1fb63f1bfb3c15528
DVKunion <2622100059@qq.com>
2022-07-15 04:40:52 +0000

    test

---
+++ src/scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker pull veinmind/veinmind-runner:latest
+docker run --rm --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/tool/resource veinmind/veinmind-runner  $*
