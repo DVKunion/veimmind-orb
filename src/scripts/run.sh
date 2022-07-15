d1fef61843e258b63e39b3a9e00564348cb44da7
DVKunion <2622100059@qq.com>
2022-07-15 05:35:15 +0000

    fix: some err

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull veinmind/veinmind-runner:latest
-path=`pwd`
+path=$(pwd)
 docker run --rm --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock -v $path:/tool/resource veinmind/veinmind-runner  ${SCAN_ACTION} ${IMAGE_REF} -o ${OUTPUT} -e ${EXIT_CODE}
