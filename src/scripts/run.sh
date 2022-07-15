60a303e5f8f8197041ccc54d36125a1af902fee1
DVKunion <2622100059@qq.com>
2022-07-15 05:30:09 +0000

    fix

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker pull veinmind/veinmind-runner:latest
-docker run --rm --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/tool/resource veinmind/veinmind-runner  ${SCAN_ACTION} ${IMAGE_REF} -o ${OUTPUT} -e ${EXIT_CODE}
+docker run --rm --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock -v $(`pwd`):/tool/resource veinmind/veinmind-runner  ${SCAN_ACTION} ${IMAGE_REF} -o ${OUTPUT} -e ${EXIT_CODE}
